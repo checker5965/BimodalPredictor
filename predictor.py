7e73793e5b893ceec9354900ded1e2f3097f7d4a
checker5965 <archit.checker_ug20@ashoka.edu.in>
2018-12-13 19:11:14 +0530

    add predictor, trace file

---
+++ predictor.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import binascii
+
+#read csv
+df = pd.read_csv('small.txt', sep=' ', header=None)
+
+#key-value pair dictionary to store address as key and 2bit sat counter and prediction next to it.
+pair = {}
+
+#list to store predictions
+predictions = []
+
+#traversing through the csv
+for addr, outcome in zip(df[0], df[1]):
+    
+    #converting address to binary
+    addr = bin(int(addr, 16))[2:]
+    
+    #extracting 14 bits from the middle of the binary addresss
+    addr = addr[4:18]
+
+    #if address is not already in dictionary, add prediction as not taken, initialize sat counter to 0
+    if addr not in pair:
+        pair[addr] = ['N', 0]
+    else:
+        #if sat counter has value greater than 2, predict taken
+        if pair[addr][1]>=2:
+            pair[addr][0] = 'T'
+        
+        #else predict not taken
+        else:
+            pair[addr][0] = 'N'
+    
+    #append predictions to list
+    predictions.append(pair[addr][0])
+
+    #since it is a saturating counter, it increments and stays at 3 even if actual outcome was taken
+    if outcome=='T' and pair[addr][1]!=3:
+        pair[addr][1]+=1
+    
+    #similarly, it decrements and stays at 0 if actual outcome was not taken
+    elif outcome=='N' and pair[addr][1]!=0:
+        pair[addr][1]-=1
+
+#counting correct predictions
+correct = 0
+c = 0
+
+#comparing outcomes and predictions
+for outcome in df[1]:
+    if predictions[c] == outcome:
+        correct += 1
+    c+=1
+print 'Accuracy: ' + str((float(correct)/df.shape[0])*100) + '%' 
